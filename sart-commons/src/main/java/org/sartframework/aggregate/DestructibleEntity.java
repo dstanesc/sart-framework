43e17f001ebc37a4abed95ccc9c78fa6bf26d193
Dragos Stanescu <dragos.stanescu@mscsoftware.com>
2019-04-22 16:46:56 -0700

    DestructibleEntity to reuse xmax tombstone pattern

---
+++ sart-commons/src/main/java/org/sartframework/aggregate/DestructibleEntity.java
@@ -0,0 +1,14 @@
+package org.sartframework.aggregate;
+
+public class DestructibleEntity {
+
+    protected Long xmax;
+
+    public Long getXmax() {
+        return xmax;
+    }
+
+    public void setXmax(Long xmax) {
+        this.xmax = xmax;
+    }
+}
